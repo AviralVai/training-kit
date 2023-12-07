eb59e56735bea5b20f8a89046363835f654fbfca
AviralVai <153048655+AviralVai@users.noreply.github.com>
2023-12-07 12:34:59 +0530

    Delete dist directory

--- dist/utils/ut_gs_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// config variables
-$app_name = 'Training-Kit';
-$home_url = 'https://dev-042023.myparag.com/';
-$app_url = $home_url.'app/common/main/main.php';
-$upload_dir='app/dist/upload/common';
-
-?>
